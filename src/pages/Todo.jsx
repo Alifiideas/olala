906ddcb96753142ed90776607407e6e75fcdabdc
Alifiideas <hmsoaib996@gmail.com>
2026-01-09 11:00:22 +0600

    Update metadata generator logic

--- src/pages/Todo.jsx
+++ src/pages/Todo.jsx
@@ -4,31 +4,28 @@ function Todo() {
   const [todos, setTodos] = useState([]);
   const [text, setText] = useState("");
 
-  const fetchTodos = async () => {
-    const res = await fetch("http://127.0.0.1:5000/todos");
-    const data = await res.json();
-    setTodos(data);
-  };
-
   useEffect(() => {
-    fetchTodos();
+    fetch("http://127.0.0.1:5000/todos")
+      .then(res => res.json())
+      .then(data => setTodos(data));
   }, []);
 
   const addTodo = async () => {
     if (!text.trim()) return;
 
-    await fetch("http://127.0.0.1:5000/todos", {
+    const res = await fetch("http://127.0.0.1:5000/todos", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text })
     });
 
-    fetchTodos();
+    const updatedTodos = await res.json();
+    setTodos(updatedTodos);
     setText("");
   };
 
   return (
-    <section style={{ padding: "40px" }}>
+    <section>
       <h1>Todo App (Python Connected)</h1>
 
       <input
